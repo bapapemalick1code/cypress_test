77864766f269dd33fe22fd1ac5c4b521d65d85fb
pape malick ba <pmba@agiltoo.fr>
2023-09-21 10:08:45 +0200

    refactoring - correction erreur (value undefined)

--- cypress/e2e/contactform.cy.js
+++ cypress/e2e/contactform.cy.js
@@ -1,39 +1,43 @@
 
 describe("MVP Testing - Fake Contact", () => {
-    let id;
-    let genre;
-    let prenom;
-    let nom;
-    let message;
-    it("Récupérer les données de test depuis l'API", () => {
+    let userData;
+
+    before(() => {
+        // Avant l'exécution des tests, récupérez les données depuis l'API
         cy.request({
-            method: 'GET',
-            url: 'https://dummyapi.io/data/v1/post',
-            headers: {
-                'Content-Type': 'text/html',
-                'app-id': '61f4248c9d9bb038eaf0c6c0',
-            },
-        })
-            .then((response) => {
-                //recuperation des données du premier User 
-                expect(response.body.data[0])
-                prenom = response.body.data[0].owner.firstName;
-                nom = response.body.data[0].owner.lastName;
-                genre = response.body.data[0].owner.title == 'ms' ? 'Femme' : 'Homme';
-                message = response.body.data[0].text;
-                id = response.body.data[0].id;
-            })
-    })
-    it("Tester le formulaire en saisissant les données obligatoires", () => {
-        //Acces au formulaire
+          method: "GET",
+          url: "https://dummyapi.io/data/v1/post",
+          headers: {
+            "Content-Type": "application/json", 
+            "app-id": "61f4248c9d9bb038eaf0c6c0",
+          },
+        }).then((response) => {
+          // Stockez les données du premier utilisateur dans la variable globale
+          userData = response.body.data[0];
+        });
+      });
+
+
+    it("Renseigner le formulaire avec les données du premier utilisateur", () => {
+        // Accédez à la page du formulaire
         cy.visit("https://testqa.purse.tech/fake-contact")
-        //Remplissage du formulaire avec les données récupérées depuis l'API
+    
+        // Vérifiez que les données du premier utilisateur ont été récupérées avec succès
+        expect(userData).to.not.be.undefined;
+        
+        // Remplissez le formulaire avec les données utilisateur
+        let genre = userData.owner.title == 'ms' ? 'Femme' : 'Homme';
         cy.get("#gender").select(genre)
-        cy.get("#first-name").type(prenom)
-        cy.get("#last-name").type(nom)
-        cy.get("#message").type(message)
-        cy.get("#submit-button").click()
-        //verification du message après l'envoi
-        cy.get("#popin-message").contains('Le message a été envoyé.')
-    })
+        cy.get("#first-name").type(userData.owner.firstName)
+        cy.get("#last-name").type(userData.owner.lastName)
+        cy.get("#message").type(userData.text)
+        // Soumettez le formulaire
+        cy.get("#submit-button").click() 
+    
+        // Effectuez des assertions pour vérifier que le formulaire a été soumis avec succès
+        cy.get("#popin-message").should("contain", "Le message a été envoyé.");
+        //cy.get("#popin-message").contains('Le message a été envoyé.')
+      });
+
+
 })
